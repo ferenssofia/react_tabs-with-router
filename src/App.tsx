8c8c75f6e19c8ea8a0ce1a576c8e54e8842927fa
Sofia Ferens <ferenssofia3009@gmail.com>
2026-08-19 16:50:14 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { Routes, Route, Link, Navigate, useLocation } from 'react-router-dom';
 import { Tabs } from './Tabs/Tabs';
 
 const tabs = [
@@ -10,62 +10,68 @@ const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <>
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `navbar-item ${isActive ? 'is-active' : ''}`
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/tabs"
-            className={({ isActive }) =>
-              `navbar-item ${isActive ? 'is-active' : ''}`
-            }
-          >
-            Tabs
-          </NavLink>
+export const App = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={`navbar-item ${pathname === '/' ? 'is-active' : ''}`}
+            >
+              Home
+            </Link>
+
+            <Link
+              to="/tabs"
+              className={`navbar-item ${
+                pathname.startsWith('/tabs') ? 'is-active' : ''
+              }`}
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1 className="title">Home page</h1>} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="tabs">
+              <Route
+                index
+                element={
+                  <>
+                    <h1 className="title">Tabs page</h1>
+                    <Tabs tabs={tabs} />
+                  </>
+                }
+              />
+              <Route
+                path=":tabId"
+                element={
+                  <>
+                    <h1 className="title">Tabs page</h1>
+                    <Tabs tabs={tabs} />
+                  </>
+                }
+              />
+            </Route>
+            <Route
+              path="*"
+              element={<h1 className="title">Page not found</h1>}
+            />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<h1 className="title">Home page</h1>} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path="tabs">
-            <Route
-              index
-              element={
-                <>
-                  <h1 className="title">Tabs page</h1>
-                  <Tabs tabs={tabs} />
-                </>
-              }
-            />
-            <Route
-              path=":tabId"
-              element={
-                <>
-                  <h1 className="title">Tabs page</h1>
-                  <Tabs tabs={tabs} />
-                </>
-              }
-            />
-          </Route>
-          <Route path="*" element={<h1 className="title">Page not found</h1>} />
-        </Routes>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
